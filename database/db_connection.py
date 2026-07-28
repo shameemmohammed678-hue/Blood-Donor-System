dca7933f0d3abc8a61adeab812b96f8d48a6641c
Mohammed Shameem <shameemmohammed678@gmail.com>
2026-07-28 22:38:11 +0530

    use absolute path for TIDB SSL certificate

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,22 +2,28 @@ import os
 import pymysql
 from config import Config
 
+import os
+import pymysql
+from config import Config
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 def get_connection():
     try:
         connection = pymysql.connect(
-        host=Config.MYSQL_HOST,
-        port=4000,
-        user=Config.MYSQL_USER,
-        password=Config.MYSQL_PASSWORD,
-        database=Config.MYSQL_DB,
-        ssl={
-        "ca": os.path.join("certs", "isrgrootx1.pem")
-         },
-        cursorclass=pymysql.cursors.DictCursor
+            host=Config.MYSQL_HOST,
+            port=4000,
+            user=Config.MYSQL_USER,
+            password=Config.MYSQL_PASSWORD,
+            database=Config.MYSQL_DB,
+            ssl={
+                "ca": os.path.join(BASE_DIR, "certs", "isrgrootx1.pem")
+            },
+            cursorclass=pymysql.cursors.DictCursor
         )
         return connection
     except Exception as e:
-        print("Database Connection Failed",e)
+        print("Database Connection Failed:", e)
         return None
            
 if __name__ == "__main__":
